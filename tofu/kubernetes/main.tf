8da871fb975989810b375bd3c8c6fb1c470a0665
Laurens Druwel <laurensdr@outlook.com>
2025-01-07 21:36:05 +0100

    update

--- tofu/kubernetes/main.tf
+++ tofu/kubernetes/main.tf
@@ -17,8 +17,8 @@ module "talos" {
   }
 
   cluster = {
-    name            = "talos"
-    endpoint        = "10.0.0.15"
+    name            = "homelab"
+    endpoint        = "10.0.0.20"
     gateway         = "10.0.0.1"
     talos_version   = "v1.9.1"
     proxmox_cluster = "homelab"
@@ -26,9 +26,9 @@ module "talos" {
 
   nodes = {
     "ctrl-00" = {
-      host_node     = "pve"
+      host_node     = "pve0"
       machine_type  = "controlplane"
-      ip            = "10.0.0.10"
+      ip            = "10.0.0.20"
       mac_address   = "BC:24:11:2E:C8:00"
       vm_id         = 800
       cpu           = 2
@@ -38,7 +38,7 @@ module "talos" {
 #    "ctrl-01" = {
 #      host_node     = "green"
 #      machine_type  = "controlplane"
-#      ip            = "10.0.0.11"
+#      ip            = "10.0.0.21"
 #      mac_address   = "BC:24:11:2E:C8:01"
 #      vm_id         = 801
 #      cpu           = 2
@@ -48,16 +48,16 @@ module "talos" {
 #    "ctrl-02" = {
 #      host_node     = "blue"
 #      machine_type  = "controlplane"
-#      ip            = "10.0.0.12"
+#      ip            = "10.0.0.22"
 #      mac_address   = "BC:24:11:2E:C8:02"
 #      vm_id         = 802
 #      cpu           = 2
 #      ram_dedicated = 4096
 #    }
     "work-00" = {
-      host_node     = "pve"
+      host_node     = "pve0"
       machine_type  = "worker"
-      ip            = "10.0.0.20"
+      ip            = "10.0.0.30"
       mac_address   = "BC:24:11:2E:A8:00"
       vm_id         = 810
       cpu           = 4
